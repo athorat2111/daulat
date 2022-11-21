e6759d381be5de9bc2caad2cf08d9c400c00aedc
Abhijit Thorat <99641490+athorat2111@users.noreply.github.com>
2022-11-22 02:01:43 +0530

    Add files via upload

---
+++ Que-02.c.sh
@@ -0,0 +1,10 @@
+echo Enter String you want to reverse
+read s
+# s="Hello World"
+strlen=${#s}
+for (( i=$strlen-1; i>=0; i-- ));
+do
+    revstr=$revstr${s:$i:1}
+done
+echo "Original String : $s"
+echo "Reversed String : $revstr"
